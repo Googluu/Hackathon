5ad1e17e8a62fdc548bd5114ebaba832354fd063
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-09 11:13:23 -0500

    register :shipit:

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom'
 
-// 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 
 import './index.css';
